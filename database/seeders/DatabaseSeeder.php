34e5aaad3dfd7c2886644abf731c0032984e84db
ahmed_ibrahim <ahmed.ibrahim@ansie.dj>
2023-06-03 14:35:05 +0300

    refactor: remove unused view and route

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Branch;
+use App\Models\Subject;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +16,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create([
-            'email' => 'admin@admin.com'
+            'email' => 'admin@admin.com',
         ]);
+
+        User::factory()
+            ->count(4)
+            ->has(Subject::factory()
+                ->for(Branch::factory()->create())
+                ->count(2)
+            )
+            ->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
